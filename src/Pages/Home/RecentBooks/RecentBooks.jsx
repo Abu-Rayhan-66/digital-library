0820c30b17b7baafaea52326d11d5515589aec03
abu-rayhan66 <mraburayhan66@gmail.com>
2023-11-09 12:25:22 +0600

    created new book section

--- src/Pages/Home/RecentBooks/RecentBooks.jsx
+++ src/Pages/Home/RecentBooks/RecentBooks.jsx
@@ -4,37 +4,37 @@ const RecentBooks = () => {
     return (
         <div>
             <div >
-            <h2 className="text-5xl font-extrabold text-center mt-10 mb-10">Discount Offer</h2>
+            <h2 className="mt-10 text-center text-3xl font-bold text-blue-500">New Books</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-10 md:mx-20 gap-6">
             
             <div >
                 <img className="rounded-md h-[40vh] w-full p-4 shadow-xl" src="https://i.ibb.co/hZqy9kg/stack-colorful-books-74855-314.jpg" alt="" />
-                <h2 className="text-center text-lg font-semibold">Samsung Galaxy S23 Is Now 10% Off.</h2>
+                <h2 className="text-center text-blue-500 text-lg font-semibold">The Sanatorium</h2>
                 
             </div>
             <div >
                 <img className="rounded-md h-[40vh] w-full p-4 shadow-xl" src="https://i.ibb.co/4tYgXJP/stack-notebooks-with-black-cover-23-2147803558.jpg" alt="" />
-                <h2 className="text-center text-lg font-semibold"> Iphone 15 Is Now 15% Off On Pre Order. </h2>
+                <h2 className="text-center text-blue-500 text-lg font-semibold">The Four Winds</h2>
                 
             </div>
             <div >
                 <img className="rounded-md h-[40vh] w-full p-4 shadow-xl" src="https://i.ibb.co/ZVTD9JR/two-books-wrappers-23-2147803590.jpg" alt="" />
-                <h2 className="text-center text-lg font-semibold">Iphone 14 Is Now 20% Off.</h2>
+                <h2 className="text-center text-blue-500 text-lg font-semibold">The Vanishing Half</h2>
                 
             </div>
             <div >
                 <img className="rounded-md h-[40vh] w-full p-4 shadow-xl" src="https://i.ibb.co/DKjwzpB/stack-books-with-word-front-1340-27740.jpg" alt="" />
-                <h2 className="text-center text-lg font-semibold">Sony Xperia 1 Is Now 8% Off  </h2>
+                <h2 className="text-center text-blue-500 text-lg font-semibold">The Last House on Needless Street</h2>
                 
             </div>
             <div >
                 <img className="rounded-md h-[40vh] w-full p-4 shadow-xl" src="https://i.ibb.co/vvBPzQH/color-books-reading-school-isolated-1308-33252.jpg" alt="" />
-                <h2 className="text-center text-lg font-semibold">OnePlus Nord Is Now 30% Off Because Of Green Line</h2>
+                <h2 className="text-center text-blue-500 text-lg font-semibold">The Rose Code</h2>
                 
             </div>
             <div >
-                <img className="rounded-md h-[40vh] w-full p-4 shadow-xl" src="https://i.ibb.co/JqRwm4S/event6.png" alt="" />
-                <h2 className="text-center text-lg font-semibold">Xiaomi 12 Pro Is Now 12% Off </h2>
+                <img className="rounded-md h-[40vh] w-full p-4 shadow-xl" src="https://i.ibb.co/HqzxgTY/stacked-books-1204-6.jpg" alt="" />
+                <h2 className="text-center text-blue-500 text-lg font-semibold">Project Hail Mary</h2>
                 
             </div>
             
