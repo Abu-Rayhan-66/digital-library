b9d325a9b1acc21fbaaffa5394ced5cd14bba556
abu-rayhan66 <mraburayhan66@gmail.com>
2023-11-09 11:36:08 +0600

    created register page

--- src/Pages/Home/RecentBooks/RecentBooks.jsx
+++ src/Pages/Home/RecentBooks/RecentBooks.jsx
@@ -3,9 +3,46 @@
 const RecentBooks = () => {
     return (
         <div>
-          
+            <div >
+            <h2 className="text-5xl font-extrabold text-center mt-10 mb-10">Discount Offer</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-10 md:mx-20 gap-6">
+            
+            <div >
+                <img className="rounded-md h-[40vh] w-full p-4 shadow-xl" src="https://i.ibb.co/1KjFC6x/event1.png" alt="" />
+                <h2 className="text-center text-lg font-semibold">Samsung Galaxy S23 Is Now 10% Off.</h2>
+                
+            </div>
+            <div >
+                <img className="rounded-md h-[40vh] w-full p-4 shadow-xl" src="https://i.ibb.co/kxKdJ2D/event2.png" alt="" />
+                <h2 className="text-center text-lg font-semibold"> Iphone 15 Is Now 15% Off On Pre Order. </h2>
+                
+            </div>
+            <div >
+                <img className="rounded-md h-[40vh] w-full p-4 shadow-xl" src="https://i.ibb.co/R31TDK5/event3.png" alt="" />
+                <h2 className="text-center text-lg font-semibold">Iphone 14 Is Now 20% Off.</h2>
+                
+            </div>
+            <div >
+                <img className="rounded-md h-[40vh] w-full p-4 shadow-xl" src="https://i.ibb.co/KWC1Njk/event4.png" alt="" />
+                <h2 className="text-center text-lg font-semibold">Sony Xperia 1 Is Now 8% Off  </h2>
+                
+            </div>
+            <div >
+                <img className="rounded-md h-[40vh] w-full p-4 shadow-xl" src="https://i.ibb.co/2c7Hdzx/event5.png" alt="" />
+                <h2 className="text-center text-lg font-semibold">OnePlus Nord Is Now 30% Off Because Of Green Line</h2>
+                
+            </div>
+            <div >
+                <img className="rounded-md h-[40vh] w-full p-4 shadow-xl" src="https://i.ibb.co/JqRwm4S/event6.png" alt="" />
+                <h2 className="text-center text-lg font-semibold">Xiaomi 12 Pro Is Now 12% Off </h2>
+                
+            </div>
+            
+        </div>
+        </div>
         </div>
     );
+    
 };
 
 export default RecentBooks;
